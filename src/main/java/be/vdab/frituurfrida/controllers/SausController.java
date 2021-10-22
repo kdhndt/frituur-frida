378203b17174f30c46a96371b0635a43bf279e25
Kasper VDAB <kasper.dehondt@vdabcampus.be>
2021-10-22 15:40:47 +0200

    einde dag

--- src/main/java/be/vdab/frituurfrida/controllers/SausController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/SausController.java
@@ -7,17 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("sauzen")
 class SausController {
     private final Saus[] sauzen = {
-            new Saus(1, "Cocktail", new String[] {"tomaat"}),
-            new Saus(2, "Mayonaise", new String[] {"ei", "citroen"}),
-            new Saus(3, "Mosterd", new String[] {"mosterdzaad"}),
-            new Saus(4, "Tartare", new String[] {"ei", "bieslook"}),
-            new Saus(5, "Vinaigrette", new String[] {"gember", "tijm"}),
+            new Saus(1, "cocktail", new String[] {"tomaat"}),
+            new Saus(2, "mayonaise", new String[] {"ei", "citroen"}),
+            new Saus(3, "mosterd", new String[] {"mosterdzaad"}),
+            new Saus(4, "tartare", new String[] {"ei", "bieslook"}),
+            new Saus(5, "vinaigrette", new String[] {"gember", "tijm"}),
 
     };
     @GetMapping
@@ -34,6 +36,34 @@ class SausController {
         Arrays.stream(sauzen).filter(saus -> saus.getNummer() == nummer).findFirst()
                 .ifPresent(saus -> modelAndView.addObject(saus));
         return modelAndView;
+    }
+
+/*    public List<String> lettersVanAlfabet() {
+        List<String> alfabet = new ArrayList<>();
+        char c;
+        for (c = 'A'; c <= 'Z'; ++c) {
+            alfabet.add(String.valueOf(c));
+        }
+        return alfabet;
+    }*/
+
+    private final char[] alfabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    @GetMapping("alfabet")
+    public ModelAndView toonPagina() {
+        return new ModelAndView("alfabet", "alfabet", alfabet);
+    }
+
+    private List<Saus> sauzenDieBeginnenMet(char letter) {
+        return Arrays.stream(sauzen)
+                .filter(saus -> saus.getNaam().charAt(0) == letter)
+                .toList();
+    }
+
+    @GetMapping("alfabet/{letter}")
+    public ModelAndView sauzenBeginnendMet(@PathVariable char letter) {
+        return new ModelAndView("alfabet", "sauzenVanEenLetter", sauzenDieBeginnenMet(letter))
+                .addObject("alfabet", alfabet);
     }
 
 }
